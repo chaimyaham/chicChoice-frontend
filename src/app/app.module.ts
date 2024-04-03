42e6a8a9e6e95017cdad84c815ee01b4cf9a2ba8
Chaimaa mahy <chaim.yaham17@gmail.com>
2024-04-03 10:28:00 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoryComponent } from './components/category/category.component';
+import { HomeSliderComponent } from './components/home-slider/home-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    PreloaderComponent,
+    HeaderComponent,
+    CategoryComponent,
+    HomeSliderComponent
   ],
   imports: [
     BrowserModule,
