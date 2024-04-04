e898bf05f57985002ae96c8648d5a7665d9824e1
Chaimaa mahy <chaim.yaham17@gmail.com>
2024-04-04 01:31:26 +0000

    adding dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/user-dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomePageComponent},
+  {path:"login", component:LoginComponent},
+  {path:"sign-up", component:SignUpComponent},
+  {path:"dashboard", component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { PreloaderComponent } from './components/preloader/preloader.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CategoryComponent } from './components/category/category.component';
 import { HomeSliderComponent } from './components/home-slider/home-slider.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/user-dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,16 @@ import { HomeSliderComponent } from './components/home-slider/home-slider.compon
     PreloaderComponent,
     HeaderComponent,
     CategoryComponent,
-    HomeSliderComponent
+    HomeSliderComponent,
+    LoginComponent,
+    HomePageComponent,
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
